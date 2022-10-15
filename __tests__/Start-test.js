317c781f68be6193d66ef38912de03515be55db8
pedrolunaa <pedroluna200207@gmail.com>
2022-10-15 20:52:06 -0300

    List of changes: - inserted unit tests

--- __tests__/Start-test.js
+++ __tests__/Start-test.js
@@ -1,8 +1,36 @@
+import { render, screen } from '@testing-library/react-native';
 import React from 'react';
-import renderer from 'react-test-renderer';
+import { create, act } from 'react-test-renderer';
 import StartScreen from '../src/views/start';
+import * as Store from '../src/redux/store/store'
 
 test('renders correctly', () => {
-  const Start = renderer.create(<StartScreen />).toJSON();
+  const isLogin = false;
+  const Start = create(
+  <Store.LoginContext.Provider value={{ isLogin }}>  
+    <StartScreen />
+  </Store.LoginContext.Provider>).toJSON();
   expect(Start).toMatchSnapshot();
+});
+
+it('login button title', () => {
+  const title = 'Entrar';
+  const isLogin = false;
+  const {getByText} = render(
+  <Store.LoginContext.Provider value={{ isLogin }}>  
+    <StartScreen />
+  </Store.LoginContext.Provider>).toJSON();
+  const foundButtonTitle = screen.getByText(title);
+  expect(foundButtonTitle.props.children).toEqual(title);
+});
+
+it('create account button title', () => {
+  const title = 'Criar uma conta';
+  const isLogin = false;
+  const {getByText} = render(
+  <Store.LoginContext.Provider value={{ isLogin }}>  
+    <StartScreen />
+  </Store.LoginContext.Provider>).toJSON();
+  const foundButtonTitle = screen.getByText(title);
+  expect(foundButtonTitle.props.children).toEqual(title);
 });
