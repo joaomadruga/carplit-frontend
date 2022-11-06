3d0e7dac16337cb3521863e0fe164518f6cb17d1
Joao Madruga <joaop050702@hotmail.com>
2022-11-06 14:16:38 -0300

    List of changes: - Changed api link to vercel

--- src/helper/api.js
+++ src/helper/api.js
@@ -2,7 +2,8 @@ import axios from 'axios';
 
 const api = axios.create({
     //baseURL: 'https://carplit-backend.herokuapp.com/',
-    baseURL: 'https://carplit-backend.up.railway.app/',
+    //baseURL: 'https://carplit-backend.up.railway.app/',
+    baseURL: 'https://carplit-backend.vercel.app/',
 });
 
 export default api;
